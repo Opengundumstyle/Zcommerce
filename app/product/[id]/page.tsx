bcd1a705eae04bfdcbcd541fccea0b527c2071a8
Zhile Lin <zhlin04@gmail.com>
2023-05-30 15:54:51 -0700

    fix id params

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+// 'use client'
 
 import Image from "next/image"
 import { SearchParamTypes } from "@/types/SearchParamTypes"
 import formatPrice from "@/app/util/PriceFormat"
 import AddCart from "./AddCart"
-import { useParams } from "next/navigation"
+// import { useParams } from "next/navigation"
 
 export default async function Product({searchParams}:SearchParamTypes){
   
    console.log('what is the search params like ?',searchParams)
-   const getParams = useParams()
-   console.log('can i see the id now',getParams?.id)
-   const id = getParams?.id as string
+  //  const getParams = useParams()
+  //  console.log('can i see the id now',getParams?.id)
+  //  const id = getParams?.id as string
     return( 
       <div className="flex flex-col xl:flex-row items-center justify-between gap-24 ">
          <Image   src={searchParams.image} 
@@ -28,8 +28,8 @@ export default async function Product({searchParams}:SearchParamTypes){
         <div className="flex gap-2">
               <p className="font-bold text-teal-700">{searchParams.unit_amount && formatPrice(searchParams.unit_amount)}</p>
         </div>
-
-          <AddCart id={id} image={searchParams.image} name={searchParams.name} unit_amount={searchParams.unit_amount} />
+ 
+          <AddCart id={searchParams.id} image={searchParams.image} name={searchParams.name} unit_amount={searchParams.unit_amount} />
 
         </div>
      </div>
