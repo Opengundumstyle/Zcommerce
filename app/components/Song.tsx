146eaff64cd6646a8d991fc9614e867376b5f102
Zhile Lin <zhlin04@gmail.com>
2023-06-19 23:15:35 -0700

    song player styling

---
+++ app/components/Song.tsx
@@ -0,0 +1,39 @@
+
+'use client'
+import { useState } from "react";
+import { BsFillPlayFill } from "react-icons/bs";
+
+const Song = ({ song, index }) => {
+  const [onHovered, setOnHovered] = useState(false);
+
+  return (
+    <div
+      key={index}
+      className={`flex items-center gap-2 cursor-pointer ${
+        onHovered ? "opacity-80" : ""
+      }`}
+      onMouseEnter={() => setOnHovered(true)}
+      onMouseLeave={() => setOnHovered(false)}
+    >
+      <div className={`w-8 text-center ${onHovered && 'text-teal-700'}`}>{index + 1}</div>
+      <div className="relative">
+        <img
+          className={`w-10 h-10 ${onHovered ? "opacity-80" : ""}`}
+          src={song.image}
+          alt={song.name}
+        />
+        {onHovered && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <BsFillPlayFill className="text-white text-2xl" />
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <h3 className="font-bold">{song.name}</h3>
+        <p className="text-sm text-gray-500">{song.artists}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
--- app/components/Songs.tsx
+++ app/components/Songs.tsx
@@ -1,22 +1,13 @@
-
+import Song from "./Song";
 
 const Songs = ({ songs }) => {
     return (
       <div className="overflow-y-auto max-h-64">
         <div className="p-4">
           {songs.map((song, index) => (
-            <div key={index} className="flex items-center gap-2">
-              <div className="w-8 text-center">{index + 1}</div>
-              <div className="flex items-base gap-4">
-                <img className="w-10 h-10" src={song.image} alt={song.name} />
-                <div>
-                  <h3 className="font-bold">{song.name}</h3>
-                  <p className="text-sm text-gray-500">{song.artists}</p>
-                </div>
-              </div>
-            
-            </div>
-          ))}
+             <Song song={song} index={index}/>
+          )
+          )}
         </div>
       </div>
     );
